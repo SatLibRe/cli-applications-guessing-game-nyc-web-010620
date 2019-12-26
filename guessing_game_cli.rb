957a00d7c15e1883a33b0508b4b9d665992f92f2
Miles Higbie <miles.higbie@gmail.com>
2019-12-26 15:31:08 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,9 +3,9 @@ def run_guessing_game
   random_number = (rand(6) + 1).to_s
   guessed_number = gets.strip.to_s
     if guessed_number == random_number
-         "You guessed the correct number!"
+        puts "You guessed the correct number!"
     elsif guessed_number == "exit"
-        return "Goodbye!"
+        puts "Goodbye!"
     end 
-  return "Sorry! The computer guessed #{random_number}."
+  puts "Sorry! The computer guessed #{random_number}."
 end 
